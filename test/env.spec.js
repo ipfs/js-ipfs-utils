57bcb6ca1b17e2b877abd204a88fa24530b969e5
Hugo Dias <hugomrdias@gmail.com>
2019-09-19 10:42:08 +0100

    fix: fix typo
    Co-Authored-By: Alex Potsides <alex@achingbrain.net>

--- test/env.spec.js
+++ test/env.spec.js
@@ -96,7 +96,7 @@ describe('env', function () {
         expect(env.isNode).to.be.false()
         break
       default:
-        expect.fail(`Could not detect end. Current env is ${process.env.AEGIR_RUNNER}`)
+        expect.fail(`Could not detect env. Current env is ${process.env.AEGIR_RUNNER}`)
         break
     }
   })
