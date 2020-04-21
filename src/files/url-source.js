727f28da32c27d670f5517932d74b2c6d00da60b
Leo <5376265+leoherzog@users.noreply.github.com>
2020-04-21 16:00:42 -0400

    feat: pass in Options Object to http Constructor (#37)
    It looks like the function was written to allow this as a parameter, but it was never used. This should allow for use cases like specifying custom headers while using `urlSource`. The `node-fetch` module falls back on [default options](https://github.com/node-fetch/node-fetch#options) if none are specified, so we shouldn't need to check and send an empty object at this step.

--- src/files/url-source.js
+++ src/files/url-source.js
@@ -3,9 +3,8 @@
 const Http = require('../http')
 
 module.exports = async function * urlSource (url, options) {
-  options = options || {}
   const http = new Http()
-  const response = await http.get(url)
+  const response = await http.get(url, options)
 
   yield {
     path: decodeURIComponent(new URL(url).pathname.split('/').pop() || ''),
