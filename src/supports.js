e45e83e1b37cf58c378e2e2709ab1c73a8f4d2d8
Hugo Dias <hugomrdias@gmail.com>
2020-04-27 11:41:32 +0100

    fix: fix getUserMedia check

--- src/supports.js
+++ src/supports.js
@@ -5,5 +5,5 @@ const globalThis = require('./globalthis')
 module.exports = {
   supportsFileReader: typeof self !== 'undefined' && 'FileReader' in self,
   supportsWebRTC: 'RTCPeerConnection' in globalThis &&
-  (typeof navigator !== 'undefined' && 'getUserMedia' in navigator)
+  (typeof navigator !== 'undefined' && typeof navigator.mediaDevices !== 'undefined' && 'getUserMedia' in navigator.mediaDevices)
 }
