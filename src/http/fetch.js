853c7cf59b221519408ab74e80adc36f8052a0e1
Irakli Gozalishvili <contact@gozala.io>
2020-07-29 15:05:37 -0700

    chore: use xhr in electron

--- src/http/fetch.js
+++ src/http/fetch.js
@@ -1,7 +1,12 @@
 'use strict'
 
-const fetch = require('node-fetch')
-
-exports.fetch = fetch
-exports.Request = fetch.Request
-exports.Headers = fetch.Headers
+// Electron has `XMLHttpRequest` and should get the browser implementation
+// instead of node.
+if (typeof XMLHttpRequest !== 'undefined') {
+  module.exports = require('./fetch.browser')
+} else {
+  const fetch = require('node-fetch')
+  exports.fetch = fetch
+  exports.Request = fetch.Request
+  exports.Headers = fetch.Headers
+}
