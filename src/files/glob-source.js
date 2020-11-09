3a7fd60af57a34dcb5c291b6b22f894a1a05a77b
Alex Potsides <alex@achingbrain.net>
2020-11-09 15:34:26 +0000

    chore: update deps (#76)

--- src/files/glob-source.js
+++ src/files/glob-source.js
@@ -6,20 +6,20 @@ const Path = require('path')
 const errCode = require('err-code')
 
 /**
-* Create an async iterator that yields paths that match requested file paths.
-*
-* @param {Iterable|AsyncIterable|String} paths File system path(s) to glob from
-* @param {Object} [options] Optional options
-* @param {Boolean} [options.recursive] Recursively glob all paths in directories
-* @param {Boolean} [options.hidden] Include .dot files in matched paths
-* @param {Array<String>} [options.ignore] Glob paths to ignore
-* @param {Boolean} [options.followSymlinks] follow symlinks
-* @param {Boolean} [options.preserveMode] preserve mode
-* @param {Boolean} [options.preserveMtime] preserve mtime
-* @param {Boolean} [options.mode] mode to use - if preserveMode is true this will be ignored
-* @param {Boolean} [options.mtime] mtime to use - if preserveMtime is true this will be ignored
-* @yields {Object} File objects in the form `{ path: String, content: AsyncIterator<Buffer> }`
-*/
+ * Create an async iterator that yields paths that match requested file paths.
+ *
+ * @param {Iterable | AsyncIterable | string} paths - File system path(s) to glob from
+ * @param {Object} [options] - Optional options
+ * @param {boolean} [options.recursive] - Recursively glob all paths in directories
+ * @param {boolean} [options.hidden] - Include .dot files in matched paths
+ * @param {Array<string>} [options.ignore] - Glob paths to ignore
+ * @param {boolean} [options.followSymlinks] - follow symlinks
+ * @param {boolean} [options.preserveMode] - preserve mode
+ * @param {boolean} [options.preserveMtime] - preserve mtime
+ * @param {boolean} [options.mode] - mode to use - if preserveMode is true this will be ignored
+ * @param {boolean} [options.mtime] - mtime to use - if preserveMtime is true this will be ignored
+ * @yields {Object} File objects in the form `{ path: String, content: AsyncIterator<Buffer> }`
+ */
 module.exports = async function * globSource (paths, options) {
   options = options || {}
 
