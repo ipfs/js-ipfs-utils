2b3ebbe86df409c1bc89d7855bf427446748b073
Irakli Gozalishvili <contact@gozala.io>
2021-01-15 07:48:14 -0800

    feat: make urlSource compatible with new ipfs.add (#53)
    Changes the return type of this `urlSource` from `AsyncIterable<{ path: string, content: AsyncIterable<Uint8Array> }>` to `{ path: string, content: AsyncIterable<Uint8Array> }` removing one layer of asynchronicity and making return be a single file as opposed to collection of files that always happen to contain one file.
    
    fixes: ipfs/js-ipfs#3195
    
    Co-authored-by: achingbrain <alex@achingbrain.net>

--- src/files/url-source.js
+++ src/files/url-source.js
@@ -1,22 +1,34 @@
 'use strict'
 
 const HTTP = require('../http')
+
 /**
+ *
  * @param {string} url
  * @param {import("../types").HTTPOptions} [options]
- * @returns {AsyncIterable<{
-    path: string;
-    content?: AsyncIterable<Uint8Array>;
-}>}
+ * @returns {{
+ *   path: string;
+ *   content?: AsyncIterable<Uint8Array>;
+ * }}
  */
-async function * urlSource (url, options) {
+const urlSource = (url, options) => {
+  return {
+    path: decodeURIComponent(new URL(url).pathname.split('/').pop() || ''),
+    content: readURLContent(url, options)
+  }
+}
+
+/**
+ *
+ * @param {string} url
+ * @param {import("../types").HTTPOptions} [options]
+ * @returns {AsyncIterable<Uint8Array>}
+ */
+async function * readURLContent (url, options) {
   const http = new HTTP()
   const response = await http.get(url, options)
 
-  yield {
-    path: decodeURIComponent(new URL(url).pathname.split('/').pop() || ''),
-    content: response.iterator()
-  }
+  yield * response.iterator()
 }
 
 module.exports = urlSource
--- test/files/url-source.spec.js
+++ test/files/url-source.spec.js
@@ -5,13 +5,14 @@
 const { expect } = require('aegir/utils/chai')
 const all = require('it-all')
 const urlSource = require('../../src/files/url-source')
-const last = require('it-last')
 const { Buffer } = require('buffer')
 
 describe('url-source', function () {
   it('can get url content', async function () {
     const content = 'foo'
-    const file = await last(urlSource(`${process.env.ECHO_SERVER}/download?data=${content}`))
+    const file = urlSource(`${process.env.ECHO_SERVER}/download?data=${content}`)
+
+    expect(file).to.have.property('path', 'download')
 
     if (file && file.content) {
       await expect(all(file.content)).to.eventually.deep.equal([Buffer.from(content)])
