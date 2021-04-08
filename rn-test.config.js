5c5c782617769c4c10bd796585038e69f0b95300
Hugo Dias <hugomrdias@gmail.com>
2021-04-08 18:02:54 +0100

    fix: add encoding polyfil

--- rn-test.config.js
+++ rn-test.config.js
@@ -5,7 +5,8 @@ module.exports = {
   runner: 'mocha',
   modules: [
     'react-native-url-polyfill',
-    'web-streams-polyfill'
+    'web-streams-polyfill',
+    'text-encoding'
   ],
   patches: [{
     path: require.resolve('react-native-polyfill-globals/patches/react-native+0.63.3.patch')
--- rn-test.require.js
+++ rn-test.require.js
@@ -2,6 +2,8 @@
 
 const { polyfill: polyfillReadableStream } = require('react-native-polyfill-globals/src/readable-stream')
 const { polyfill: polyfillURL } = require('react-native-polyfill-globals/src/url')
+const { polyfill: polyfillEncoding } = require('react-native-polyfill-globals/src/encoding')
 
 polyfillURL()
 polyfillReadableStream()
+polyfillEncoding()
