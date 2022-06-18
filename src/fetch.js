45ed9767f62f62984c96ff59264cce74a018e647
Alex Potsides <alex@achingbrain.net>
2022-06-18 22:30:22 +0100

    fix: support vite
    Doing the require in this fashion seems to work better with vite.

--- src/fetch.js
+++ src/fetch.js
@@ -2,9 +2,11 @@
 
 const { isElectronMain } = require('./env')
 
-if (isElectronMain) {
-  module.exports = require('electron-fetch')
-} else {
 // use window.fetch if it is available, fall back to node-fetch if not
-  module.exports = require('native-fetch')
+let fetch = 'native-fetch'
+
+if (isElectronMain) {
+  fetch = 'electron-fetch'
 }
+
+module.exports = require(fetch)
